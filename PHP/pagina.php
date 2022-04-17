7dec0ff58f37a2bb2e76923fc64506abfe16f71b
lucianopsp <lucianopspcom@gmail.com>
2022-04-17 11:37:52 -0300

    Inclusao de pasta PHP

---
+++ PHP/pagina.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Meu site PHP.</title>
+	</head>
+	<body>
+		<?php
+			for ($i = 0 ; $i < 10 ; $i++) {
+				print("linha numero " . $i . "<br />");
+			}
+		?>
+	</body>
+</html>
