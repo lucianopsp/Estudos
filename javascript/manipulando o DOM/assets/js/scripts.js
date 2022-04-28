dc5816a9b3db1713d74d2b52f93de2bd80947b3d
lucianopsp <lucianopspcom@gmail.com>
2022-04-28 15:13:59 -0300

    Adição de novos exercícios de js e reactjs

---
+++ javascript/manipulando o DOM/assets/js/scripts.js
@@ -0,0 +1,31 @@
+// const mode = 'dark-mode';
+// const lModeText = "Light Mode";
+// const dModeText = "Dark Mode";
+// const h1 = document.getElementById('page-title');
+// const body = document.getElementsByTagName('body')[0];
+// const button = document.getElementById('mode-selector');
+// const footer = document.getElementsByTagName('footer')[0];
+
+// button.addEventListener('click', changeMode);
+
+
+// function changeMode(){
+//     changeClass();
+//     changeText();
+// }
+
+// function changeClass(){
+//     body.classList.toggle(mode);
+//     footer.classList.toggle(mode);
+//     button.classList.toggle(mode);
+// }
+
+// function changeText(){
+//     if(body.classList.contains(mode)){
+//         button.innerHTML = lModeText;
+//         h1.innerHTML = dModeText + " ON";
+//         return;
+//     }
+//     button.innerHTML = dModeText;
+//     h1.innerHTML = lModeText + " ON";
+// }
